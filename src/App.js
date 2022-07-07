14ee64a265b2a77b81aaaeaa87dce061deb940fd
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-07-07 12:01:46 -0400

    final bug fixes

--- src/App.js
+++ src/App.js
@@ -7,25 +7,38 @@ import "./App.css";
 const urlEndpoint = "http://localhost:4000";
 
 const App = () => {
-  const [serverJSON, setServerJSON] = useState({ message: null });
+  const [serverJSON, setServerJSON] = useState({ message: [] });
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+  //     const apiJSON = await apiResponse.json();
+  //     setServerJSON(apiJSON);
+  //     return;
+  //   };
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-      const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+      const url = `${urlEndpoint}/blogs/all-blogs`;
+      const apiResponse = await fetch(url);
       const apiJSON = await apiResponse.json();
       setServerJSON(apiJSON);
       return;
     };
     fetchData();
   }, []);
-  console.log(serverJSON);
+
+  // console.log(serverJSON);
+
   return (
     <div className="app">
       <header className="app-header">
         <Routes>
           <Route
             path="/blogs"
-            element={<BlogsPage message={serverJSON.message} />}
+            element={<BlogsPage blogs={serverJSON.message} />}
           />
         </Routes>
       </header>
--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,10 +1,58 @@
 import React from "react";
 
-const BlogsPage = (props) => {
+const BlogsPage = ({ blogs }) => {
   return (
     <div className="blogs-page">
       <h1>Blogs Page</h1>
-      <p>Server Message: {props.message}</p>
+      <div>
+        {blogs.map((blog) => {
+          return <BlogPost blog={blog} key={blog.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const BlogPost = ({ blog }) => {
+  return (
+    <div className="blogPost">
+      <br />
+      <span>
+        <strong> Title: </strong> &nbsp;
+        {blog.title}
+      </span>
+      <p>
+        <span>
+          <strong> Author: </strong> &nbsp;
+          {blog.author}
+        </span>
+      </p>
+      <p>
+        <span>
+          <strong>Category: </strong> &nbsp;
+          {blog.category}
+        </span>
+      </p>
+      <p>{blog.text}</p>
+      <p>
+        <span>
+          <strong> Created At: </strong> <br />
+        </span>
+        {blog.createdAt}
+      </p>
+      <p>
+        <span>
+          <strong> Last Modified: </strong> <br />
+        </span>
+        {blog.lastModified}
+      </p>
+      <p>
+        <span>
+          <strong> ID: </strong> &nbsp; {blog.id}
+        </span>
+      </p>
+      <br />
+      <hr></hr>
     </div>
   );
 };
